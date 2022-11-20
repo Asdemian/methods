a523186ef7c812f170627af58823801e5465fe55
hw commit <demaniks@yahoo.com>
2022-11-20 13:23:49 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        //Task1
+        int year = 1902;
+        boolean leapYear = leap(year);
+        conclusionYear(year, leapYear);
+    }
 
+    private static boolean leap(int year) {
+        return year % 4 == 0 || year % 400 == 0 && year % 100 == 0;
+
+    }
+
+    private static void conclusionYear(int year, boolean leapYear) {
+        if (leapYear) {
+            System.out.println(year + " Високосный год");
+        } else
+            System.out.println(year + " Не високосный год");
     }
 }
